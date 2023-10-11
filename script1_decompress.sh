92187a63de8850f884cad258bef060b662bb322d
B246942-2023 <s2478350@bioinfmsc8.bio.ed.ac.uk>
2023-10-11 14:49:00 +0100

    a faster version of script1 using multithreads

--- script1_decompress.sh
+++ script1_decompress.sh
@@ -8,13 +8,26 @@ cp -r /localdisk/data/BPSM/ICA1/fastq .
 mkdir -p fastq_decompress
 #make a dir for output
 
+thread_max=200 # Woo, an AMD 128 cores 256 threads processor! Never seen before LOL ,let's make it worth!
+thread_now=0
+#I want to use multithreads to fast it. single thread need 10 mins OMG too slow!
+
 input_dir=fastq/*fq.gz
 output_dir=fastq_decompress
 #chose where i want to input and output my data
 
 for file in $input_dir;do
     filename=$(basename $file)
-    fastqc -t 4 $file -o $output_dir #-t :more threads make it faster
+    {
+        fastqc -t 2 $file -o $output_dir #-t 2 :more threads make it faster
+    }& 
+    ((thread_now+=2)) # need two threads for fastqc each loop
+    
+    if ((thread_now >= thread_max ));then #this loop just to make sure our threads would not tooo much 
+        wait
+        thread_now=0
+    fi
 done
+wait
 
 
