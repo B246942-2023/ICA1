140bcfd589f373c7959998d3214fce93106193b5
B246942-2023 <s2478350@bioinfmsc8.bio.ed.ac.uk>
2023-10-11 23:24:40 +0100

    A more concise code of output extract summary.txt by using unzip -p

--- script2_qulitycheck.sh
+++ script2_qulitycheck.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/bash
 
 rm -rf qualitysummary
+mkdir -p qualitysummary
+#need a dir to save my data, make sure it has no data before 
+
 input_dir=fastq_decompress/*.zip 
 
 for file in $input_dir; do
     filename_origin=$(basename $file .zip) # file nameis like this:Tco-999_2_fastqc.zip > Tco-999_2_fastqc
     filename_output=${filename_origin/_fastqc/_summary.txt} #I want it be Tco-999_2_summary.txt
-    unzip -j $file $filename_origin/summary.txt -d qualitysummary #-j will not presever the structure of data
-    mv qualitysummary/summary.txt qualitysummary/$filename_output  # change it's name
+    unzip -jp $file $filename_origin/summary.txt >qualitysummary/$filename_output #-j will not presever the structure of data -p can rename it.
 done
